e46185bf94b946e8d8052225969452de33aee94b
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2022-03-29 13:00:01 +0300

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 const section = document.querySelector('.photo-grid');
 const container = section.querySelector('.photo-grid__container');
 const items = container.querySelectorAll('.photo-grid__item');
@@ -11,82 +10,77 @@ const nickNames = section.querySelectorAll('.photo-grid__nickname');
 const paragraph = section.querySelector('.photo-grid__paragraph');
 const authorPicture = section.querySelector('.photo-grid__author-pic');
 
-
 let currentIndex = 0;
 showSlides(currentIndex);
 
 function showSlides(n) {
-  console.log('nickName', nickNames.length);
 
-  items.forEach(item => {
-    item.setAttribute('style', 'order: 1');
-  })
+    items.forEach((item) => {
+        item.setAttribute('style', 'order: 1');
+    });
 
-  nickNames.forEach(item => {
-    item.classList.remove('photo-grid__nickname_active');
-  })
+    nickNames.forEach((item) => {
+        item.classList.remove('photo-grid__nickname_active');
+    });
 
-  paragraph.textContent = '';
+    paragraph.textContent = '';
 
+    let index = n;
+    let prevIndex = index - 1;
+    let nextIndex = index + 1;
 
-  let index = n;
-  let prevIndex = index-1;
-  let nextIndex = index+1;
+    if (n === items.length - 1) {
+        currentIndex = items.length - 1;
+        index = currentIndex;
+        prevIndex = index - 1;
+        nextIndex = 0;
+    }
+    if (n > items.length - 1) {
+        currentIndex = 0;
+        index = currentIndex;
+        prevIndex = items.length - 1;
+        nextIndex = index + 1;
+    }
+    if (n === 0) {
+        console.log('hi');
+        currentIndex = 0;
+        index = currentIndex;
+        prevIndex = items.length - 1;
+        nextIndex = index + 1;
+    }
+    if (n < 0) {
+        currentIndex = items.length - 1;
+        index = currentIndex;
+        nextIndex = 0;
+        prevIndex = index - 1;
+    }
 
+    // console.log("prevIndex", prevIndex);
+    // console.log("currentIndex",index);
+    // console.log("nextIndex", nextIndex);
 
+    items[prevIndex].setAttribute('style', 'order: -2');
+    items[index].setAttribute('style', 'order: -1');
+    items[nextIndex].setAttribute('style', 'order: 0');
+    nickNames[index].classList.add('photo-grid__nickname_active');
+    paragraph.textContent = initialCards[index].text;
+    authorPicture.src = `./assets/images/${initialCards[index].photo}`;
 
-  if (n === items.length-1) {
-    currentIndex = items.length-1;
-    index = currentIndex;
-    prevIndex = index - 1;
-    nextIndex = 0;
-  }
-  if (n > items.length-1) {
-    currentIndex = 0;
-    index = currentIndex;
-    prevIndex = items.length-1;
-    nextIndex = index+1;
-  }
-  if(n === 0){
-    console.log('hi')
-    currentIndex = 0
-    index = currentIndex;
-    prevIndex = items.length-1;
-    nextIndex = index + 1;
-  }
-  if (n < 0) {
-    currentIndex = items.length-1
-    index = currentIndex;
-    nextIndex = 0;
-    prevIndex = index-1;
-  }
-
-  // console.log("prevIndex", prevIndex);
-  // console.log("currentIndex",index);
-  // console.log("nextIndex", nextIndex);
-
-  items[prevIndex].setAttribute('style','order: -2');
-  items[index].setAttribute('style','order: -1');
-  items[nextIndex].setAttribute('style','order: 0');
-  nickNames[index].classList.add('photo-grid__nickname_active');
-  paragraph.textContent = initialCards[index].text;
-  authorPicture.src = `./assets/images/${initialCards[index].photo}`;
-
-  // console.log(prevIndex, getComputedStyle(items[prevIndex]).order);
-  // console.log(getComputedStyle(items[index]).order);
-  // console.log(getComputedStyle(items[nextIndex]).order);
+    // console.log(prevIndex, getComputedStyle(items[prevIndex]).order);
+    // console.log(getComputedStyle(items[index]).order);
+    // console.log(getComputedStyle(items[nextIndex]).order);
 }
 
 nextBtn.addEventListener('click', (e) => {
-  console.log('hi');
-  console.log('inner', currentIndex);
-  showSlides(currentIndex);
-  currentIndex += 1
-  console.log('outer', currentIndex);
-})
+    console.log('hi');
+    console.log('inner', currentIndex);
+    showSlides(currentIndex);
+    currentIndex += 1;
+    console.log('outer', currentIndex);
+});
 
 prevBtn.addEventListener('click', (e) => {
-  console.log('hi1');
-  showSlides(currentIndex);
-  currentIndex -= 1;
-})
+    console.log('hi1');
+    showSlides(currentIndex);
+    currentIndex -= 1;
+});
